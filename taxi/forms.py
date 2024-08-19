29cd9b72acdfa8e3a1e828ee5aa16bdd8866161d
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-08-19 09:30:04 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -8,20 +8,20 @@ from taxi.models import Driver, Car
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
-        fields = ["license_number",]
+        fields = ["license_number", ]
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
             raise ValidationError("License_number must be 8 characters long")
         if (not license_number[:3].isupper()
-            or not license_number[:3].isalpha()):
+                or not license_number[:3].isalpha()):
             raise ValidationError(
-                "Invalid license number: First 3 characters are uppercase letters"
+                "Invalid license number: First 3 char. are uppercase letters"
             )
         if not license_number[3:].isdigit():
             raise ValidationError(
-                "Invalid license number: Last 5 characters are digits"
+                "Invalid license number: Last 5 char. are digits"
             )
         return license_number
 
@@ -36,13 +36,13 @@ class DriverForm(forms.ModelForm):
         if len(license_number) != 8:
             raise ValidationError("License_number must be 8 characters long")
         if (not license_number[:3].isupper()
-            or not license_number[:3].isalpha()):
+                or not license_number[:3].isalpha()):
             raise ValidationError(
-                "Invalid license number: First 3 characters are uppercase letters"
+                "Invalid license number: First 3 char. are uppercase letters"
             )
         if not license_number[3:].isdigit():
             raise ValidationError(
-                "Invalid license number: Last 5 characters are digits"
+                "Invalid license number: Last 5 char. are digits"
             )
         return license_number
 
--- taxi/urls.py
+++ taxi/urls.py
@@ -47,8 +47,12 @@ urlpatterns = [
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/create", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/delete", DriverDeleteView.as_view(), name="driver-delete"),
-    path("drivers/<int:pk>/update", DriverUpdateView.as_view(), name="driver-update"),
+    path("drivers/<int:pk>/delete",
+         DriverDeleteView.as_view(),
+         name="driver-delete"),
+    path("drivers/<int:pk>/update",
+         DriverUpdateView.as_view(),
+         name="driver-update"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
